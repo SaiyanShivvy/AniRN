9d6b3ba238cfafeb907b004bc14263116ee3dcf3
Shivneel Achari <acharishivvy@yahoo.co.nz>
2024-03-23 05:54:45 +1300

    Rebuild Project with New Architecture for Android and iOS. MacOS and Windows builds seem to be broken.

---
+++ windows/AniRN/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::AniRN::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::AniRN::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
